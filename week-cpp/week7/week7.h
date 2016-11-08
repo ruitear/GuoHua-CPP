b61e561ea3d07235475e6c072b386922c4794ffb
ruitear <liurui849058284@qq.com>
2016-11-08 21:57:12 +0800

    week7

--- week-cpp/week7/week7.h
+++ week-cpp/week7/week7.h
@@ -50,3 +50,39 @@ const T& myStack<T>::min() const
 	assert(stack1.size() > 0 && stack2.size() > 0);
 	return stack2.top();
 }
+//day2
+//利用构造函数
+class temp{
+public:
+	temp(){ ++N; Sum += N; }
+	static void reset(){ N = 0; Sum = 0; }
+	static unsigned int GetSum(){ return Sum; }
+
+private:
+	static unsigned int N;
+	static unsigned int Sum;
+};
+unsigned int temp::N = 0;
+unsigned int temp::Sum = 0;
+
+unsigned int sumOneToN(unsigned int n)
+{
+	temp::reset();
+	temp *a = new temp[n];
+	delete[] a;
+	a = NULL;
+	return temp::GetSum();
+}
+//利用函数指针
+//定义两个函数一个充当递归的角色一个，充当借宿递归的角色
+typedef unsigned int(*fun)(unsigned int);
+unsigned int solution(unsigned int n)
+{
+	return 0;
+}
+unsigned int sumOneToN1(unsigned int n)
+{
+	static fun f[2] = { solution, sumOneToN1 };
+	cout << !n<<"--"<< (!!n) << endl;
+	return n+f[!!n](n - 1);
+}
