ee669174bcab24ee87709f36b3bae0a062145de1
ruitear <liurui849058284@qq.com>
2016-11-14 20:49:22 +0800

    week8

---
+++ week-cpp/week8/main.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//class TT{
+//public:
+//	int operator()(int i)
+//	{
+//		return ++i;
+//	}
+//};
+//int main()
+//{
+//	int i = 0;
+//	cout << TT()(i) << endl;
+//	return 0;
+//}
---
+++ week-cpp/week8/main8.cpp
@@ -0,0 +1,6 @@
+#include"week8.h"
+
+int main()
+{
+	return 0;
+}
